fc7715837b3c988040eda020cacb7708835d9139
Hannah Howard <hannah@hannahhoward.net>
2014-02-23 01:10:21 -0800

    Fix test suite

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,4 +6,7 @@ describe Category do
   it { should have_db_column(:rgt) }
   it { should have_db_column(:parent_id) }
   it { should have_many(:questions) }
+
+  describe "all questions" do
+  end
 end
