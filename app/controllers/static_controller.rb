438f58a5b3b00ab8e31b3a2ab3ae9aec47112de1
Hannah Howard <hannah@hannahhoward.net>
2014-02-24 23:03:08 -0800

    Add latest activity

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,12 @@
 class StaticController < ApplicationController
   def index
+    @activities = last_activities(5)
   end
+
+  def last_activities(num)
+    questions = Question.order("created_at DESC").limit(num)
+    answers = Answer.order("created_at DESC").limit(num)
+    (questions + answers).sort_by(&:created_at).reverse.first(num)
+  end
+
 end
